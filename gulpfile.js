08cebe8cc7162bb7a96cace1be438bb86afcee96
bxav <xavier.b@zoho.com>
2015-05-09 11:30:33 +0200

    Install gulp and gulp-sass

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+gulp.task('default', function() {
+    gulp.src('app/Resources/assets/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('web/css'));
+});
