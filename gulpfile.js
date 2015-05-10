3ebc23d532d4ce76e3f005a567b90db4ba839428
bxav <xavier.b@zoho.com>
2015-05-10 17:18:52 +0200

    Set up gulp base config with symfony

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,112 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-gulp.task('default', function() {
-    gulp.src('app/Resources/assets/sass/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('web/css'));
+var plugins = require('gulp-load-plugins')();
+var del = require('del');
+var Q = require('q');
+var config = {
+    assetsDir: 'app/Resources/assets',
+    sassPattern: 'sass/**/*.scss',
+    production: !!plugins.util.env.production,
+    sourceMaps: !plugins.util.env.production,
+    bowerDir: 'vendor/bower_components',
+    revManifestPath: 'app/Resources/assets/rev-manifest.json'
+};
+var app = {};
+app.addStyle = function(paths, outputFilename) {
+    return gulp.src(paths)
+        .pipe(plugins.plumber())
+        .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
+        .pipe(plugins.sass())
+        .pipe(plugins.concat('css/'+outputFilename))
+        .pipe(plugins.if(config.production, plugins.minifyCss()))
+        .pipe(plugins.rev())
+        .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
+        .pipe(gulp.dest('web'))
+        // write the rev-manifest.json file for gulp-rev
+        .pipe(plugins.rev.manifest(config.revManifestPath, {
+            merge: true
+        }))
+        .pipe(gulp.dest('.'));
+};
+app.addScript = function(paths, outputFilename) {
+    return gulp.src(paths)
+        .pipe(plugins.plumber())
+        .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
+        .pipe(plugins.concat('js/'+outputFilename))
+        .pipe(plugins.if(config.production, plugins.uglify()))
+        .pipe(plugins.rev())
+        .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
+        .pipe(gulp.dest('web'))
+        // write the rev-manifest.json file for gulp-rev
+        .pipe(plugins.rev.manifest(config.revManifestPath, {
+            merge: true
+        }))
+        .pipe(gulp.dest('.'));
+};
+app.copy = function(srcFiles, outputDir) {
+    gulp.src(srcFiles)
+        .pipe(gulp.dest(outputDir));
+};
+var Pipeline = function() {
+    this.entries = [];
+};
+Pipeline.prototype.add = function() {
+    this.entries.push(arguments);
+};
+Pipeline.prototype.run = function(callable) {
+    var deferred = Q.defer();
+    var i = 0;
+    var entries = this.entries;
+    var runNextEntry = function() {
+        // see if we're all done looping
+        if (typeof entries[i] === 'undefined') {
+            deferred.resolve();
+            return;
+        }
+        // pass app as this, though we should avoid using "this"
+        // in those functions anyways
+        callable.apply(app, entries[i]).on('end', function() {
+            i++;
+            runNextEntry();
+        });
+    };
+    runNextEntry();
+    return deferred.promise;
+};
+gulp.task('styles', function() {
+    var pipeline = new Pipeline();
+    pipeline.add([
+        config.bowerDir+'/bootstrap/dist/css/bootstrap.css',
+        config.bowerDir+'/font-awesome/css/font-awesome.css',
+        config.assetsDir+'/sass/layout.scss',
+        config.assetsDir+'/sass/styles.scss'
+    ], 'main.css');
+    pipeline.add([
+        config.assetsDir+'/sass/dinosaur.scss'
+    ], 'dinosaur.css');
+    pipeline.run(app.addStyle);
 });
+gulp.task('scripts', function() {
+    var pipeline = new Pipeline();
+    pipeline.add([
+        config.bowerDir+'/jquery/dist/jquery.js',
+        config.assetsDir+'/js/main.js'
+    ], 'site.js');
+    pipeline.run(app.addScript);
+});
+gulp.task('fonts', function() {
+    app.copy(
+        config.bowerDir+'/font-awesome/fonts/*',
+        'web/fonts'
+    );
+});
+gulp.task('clean', function() {
+    del.sync(config.revManifestPath);
+    del.sync('web/css/*');
+    del.sync('web/js/*');
+    del.sync('web/fonts/*');
+});
+gulp.task('watch', function() {
+    gulp.watch(config.assetsDir+'/'+config.sassPattern, ['styles']);
+    gulp.watch(config.assetsDir+'/js/**/*.js', ['scripts']);
+});
+gulp.task('default', ['clean', 'styles', 'scripts', 'fonts', 'watch']);
