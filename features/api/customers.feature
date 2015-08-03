1d22b9100ec290f7a231909dc62a65c097779bb9
bxav <xavier.b@zoho.com>
2015-08-03 15:15:28 +0200

    Add Post Customer

--- features/api/customers.feature
+++ features/api/customers.feature
@@ -1,1 +1,43 @@
 Feature: Access to the api
+
+  Background:
+    Given there is 1 business like:
+      | name |
+      | Haircut Master |
+    Given the following employee:
+      | username | plainPassword | roles | enabled | firstname | lastname | business |
+      | user | user | ROLE_API | true | marie | dupond | Haircut Master |
+    Given I specified the following request http basic credentials:
+      | username | user |
+      | password | user |
+
+  Scenario: Get customer
+    Given there is 1 customer like:
+      | username | firstname | lastname |
+      | customer | John | Duff |
+    Given I prepare a GET request on "/api/customers/customer"
+    When I send the request
+    Then print the last response
+    Then I should receive a 200 json response
+    And the properties exist:
+    """
+    firstname
+    lastname
+    username
+    """
+
+
+  Scenario: Post customer
+    Given I specified the following request body:
+    """
+    {
+        "username":"bob",
+        "firstname":"bob",
+        "lastname":"duff",
+        "email":"janne@example.com"
+    }
+    """
+    Given I prepare a POST request on "/api/customers"
+    When I send the request
+    Then print the last response
+    Then I should receive a 201 json response
---
+++ src/AppBundle/Controller/Api/CustomerController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Controller\Api;
+
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+
+
+class CustomerController extends FOSRestController implements ClassResourceInterface
+{
+
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Return a collection Customer",
+     * )
+     */
+    public function cgetAction()
+    {
+        $customers = $this->getDoctrine()->getRepository("AppBundle:Customer")->findAll();
+
+        $view = $this->view($customers, 200);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Return a Customer",
+     * )
+     */
+    public function getAction($username)
+    {
+        $customer = $this->getDoctrine()->getRepository("AppBundle:Customer")->findOneByUsername($username);
+
+        $view = $this->view($customer, 200);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Create a customer",
+     *  input="AppBundle\Entity\Booking",
+     * )
+     */
+    public function postAction(Request $request)
+    {
+
+        $data = $request->getContent();
+
+        $customer = $this->get("serializer")->deserialize($data, 'AppBundle\Entity\Customer', 'json');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($customer);
+        $em->flush();
+
+        $view = $this->view($customer, 201);
+        return $this->handleView($view);
+    }
+}
