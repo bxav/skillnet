a7177fd105990bf9eb05edde76dab1df5c72c284
bxav <xavier.b@zoho.com>
2015-08-06 11:02:49 +0200

    Clean up employees tabs

--- app/Resources/assets/js/directives/employeesTabs.js
+++ app/Resources/assets/js/directives/employeesTabs.js
@@ -8,18 +8,6 @@ angular.module('beauty')
                         $scope.employees = employees;
                     });
                 });
-
-                $scope.checkModel = $scope.employees;
-                $scope.checkResults = [];
-
-                $scope.$watchCollection('checkModel', function () {
-                    $scope.checkResults = [];
-                    angular.forEach($scope.checkModel, function (value) {
-                        if (value) {
-                            $scope.checkResults.push(value);
-                        }
-                    });
-                });
             },
             templateUrl: 'views/dashboard/employees-tabs.html'
         };
