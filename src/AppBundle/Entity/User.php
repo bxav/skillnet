d9f8deeb92ed5effe8b156ae60f2e9e9061f7b41
bxav <xavier.b@zoho.com>
2015-08-10 10:43:36 +0200

    Update fos user bundle

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,7 +4,7 @@ namespace AppBundle\Entity;
 
 
 use Doctrine\Common\Collections\ArrayCollection;
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
 
