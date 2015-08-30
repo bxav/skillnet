8c58e825744e69c0a17d2dcb3ca77dff766dbeb3
bxav <xavier.b@zoho.com>
2015-08-30 10:00:46 +0200

    Add success handler to redirect users

---
+++ src/AppBundle/Authentication/SuccessHandler.php
@@ -0,0 +1,57 @@
+<?php
+namespace AppBundle\Authentication;
+
+use Symfony\Component\Routing\RouterInterface,
+    Symfony\Component\HttpFoundation\RedirectResponse,
+    Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface,
+    Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class SuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    protected $router;
+
+    protected $backEndRoles = [];
+
+    protected $appProRoles = [];
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function setBackendUserRoles(array $roles)
+    {
+        $this->backEndRoles = $roles;
+    }
+
+    public function setAppProUserRoles(array $roles)
+    {
+        $this->appProRoles = $roles;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        if ($this->isABackendUser($token->getUser())) {
+            return new RedirectResponse($this->router->generate('sonata_admin_dashboard'));
+        } else if ($this->isAnAppProUser($token->getUser())) {
+            return new RedirectResponse($this->router->generate('pro'));
+        } else {
+            return new RedirectResponse($this->router->generate('homepage'));
+        }
+    }
+
+    protected function isABackendUser($user)
+    {
+        $roles = $user->getRoles();
+        $roles = array_intersect($this->backEndRoles, $roles);
+        return (bool) $roles;
+    }
+
+    protected function isAnAppProUser($user)
+    {
+        $roles = $user->getRoles();
+        $roles = array_intersect($this->appProRoles, $roles);
+        return (bool) $roles;
+    }
+}
