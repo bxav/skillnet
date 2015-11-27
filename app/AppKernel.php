4b3dba4ec32ef79b4880232ee159b5e0b31fb105
bxav <xavier.b@zoho.com>
2015-11-27 13:20:51 +0100

    Rm useless client

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -60,7 +60,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Bxav\Bundle\BellApiClientBundle\BxavBellApiClientBundle();
         }
 
         return $bundles;
--- src/Bxav/Bundle/BellApiClientBundle/BxavBellApiClientBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Bxav\Bundle\BellApiClientBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BxavBellApiClientBundle extends Bundle
-{
-}
