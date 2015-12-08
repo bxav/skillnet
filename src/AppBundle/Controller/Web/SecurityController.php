bc74c44e03380d6c9fd5314e748e86703d420365
bxav <xavier.b@zoho.com>
2015-12-08 13:52:05 +0100

    Update auth

--- src/AppBundle/Controller/Web/SecurityController.php
+++ src/AppBundle/Controller/Web/SecurityController.php
@@ -18,11 +18,9 @@ use Symfony\Component\Security\Core\Security;
 
 class SecurityController extends Controller
 {
-    /**
-     * @Route("/login", name="login_route")
-     */
     public function loginAction(Request $request)
     {
+
         $authenticationUtils = $this->get('security.authentication_utils');
 
         // get the login error if there is one
@@ -34,19 +32,14 @@ class SecurityController extends Controller
         return $this->render(
             'security/login.html.twig',
             array(
-                // last username entered by the user
                 'last_username' => $lastUsername,
                 'error' => $error,
             )
         );
     }
 
-    /**
-     * @Route("/login_check", name="login_check")
-     */
-    public function loginCheckAction()
+    public function loginCheckAction(Request $request)
     {
-        // this controller will not be executed,
-        // as the route is handled by the Security system
+
     }
 }
